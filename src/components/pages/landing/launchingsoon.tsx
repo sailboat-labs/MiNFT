05ceed844f84b849d4fcca112778c8c22122a4a4
Raymond Tetteh <raymondtetteh111@gmail.com>
2022-03-03 19:46:07 +0000

    table

--- src/components/pages/landing/launchingsoon.tsx
+++ src/components/pages/landing/launchingsoon.tsx
@@ -1,86 +1,40 @@
+import {
+  collection,
+  DocumentData,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import { firebaseApp } from "@/lib/firebase";
+
+import { Collection } from "@/types";
+
+const firestore = getFirestore(firebaseApp);
 export default function LaunchingSoon() {
-  const collections: {
-    name: string;
-    image: string;
-    mintDate: string;
-    supply: string;
-    whitelist: boolean;
-    teamInfo: boolean;
-    projectType: string;
-  }[] = [
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-    {
-      name: "Azuki",
-      image: "",
-      supply: "10000",
-      mintDate: "Tomorrow",
-      whitelist: true,
-      teamInfo: false,
-      projectType: "ART",
-    },
-  ];
+  const [collections, setCollections] = useState<Collection[]>([]);
+
+  const _query = query(
+    collection(firestore, "collections"),
+    orderBy("lastUpdated", "desc"),
+    limit(100)
+  );
+  const [snapshots, loading] = useCollectionData(_query);
+
+  useEffect(() => {
+    if (loading) return;
+    if (!snapshots) return;
+
+    const data = snapshots.reduce((acc: Collection[], curr: DocumentData) => {
+      acc.push(curr as Collection);
+      return acc;
+    }, []);
+
+    setCollections(data);
+  }, [loading, snapshots]);
 
   return (
     <div className="contained mt-10">
@@ -124,11 +78,12 @@ export default function LaunchingSoon() {
                   </tr>
                 </thead>
                 <tbody>
-                  {collections.map((collection, index) => (
-                    <tr key={index} className="border-b bg-white ">
-                      <td className="flex items-center gap-5 whitespace-nowrap py-4 px-6 text-sm font-medium text-gray-900">
-                        <div className="h-10 w-10 flex-shrink-0 rounded-[50%] bg-gray-100">
-                          {/* <img
+                  {collections &&
+                    collections.map((collection, index) => (
+                      <tr key={index} className="border-b bg-white ">
+                        <td className="flex items-center gap-5 whitespace-nowrap py-4 px-6 text-sm font-medium text-gray-900">
+                          <div className="h-10 w-10 flex-shrink-0 rounded-[50%] bg-gray-100">
+                            {/* <img
                           className='h-full w-full rounded-[50%] object-cover'
                           src={
                             limitedPartner.profileImage ??
@@ -136,29 +91,31 @@ export default function LaunchingSoon() {
                           }
                           alt=''
                         /> */}
-                        </div>
-                        <div>
-                          <div className="text-md">{collection.name}</div>
-                          <div className="whitespace-nowrap text-sm text-gray-500 ">
-                            <span>{collection.supply}</span>
-                            <span>&nbsp;circulating supply</span>
                           </div>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 text-sm text-gray-500 ">
-                        {collection.mintDate}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 text-sm uppercase text-gray-500">
-                        {collection.whitelist.toString()}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 text-sm uppercase text-gray-500">
-                        {collection.teamInfo.toString()}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 text-sm text-gray-500 ">
-                        {collection.projectType}
-                      </td>
-                    </tr>
-                  ))}
+                          <div>
+                            <div className="text-md">{collection.name}</div>
+                            <div className="whitespace-nowrap text-sm text-gray-500 ">
+                              <span>{collection.supply}</span>
+                              <span>&nbsp;circulating supply</span>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap py-4 px-6 text-sm text-gray-500 ">
+                          {collection.name}
+                        </td>
+                        <td className="whitespace-nowrap py-4 px-6 text-sm uppercase text-gray-500">
+                          {collection.whitelistAvailable == "yes"
+                            ? "true"
+                            : "false"}
+                        </td>
+                        <td className="whitespace-nowrap py-4 px-6 text-sm uppercase text-gray-500">
+                          {collection.teamInfo}
+                        </td>
+                        <td className="whitespace-nowrap py-4 px-6 text-sm text-gray-500 ">
+                          {collection.projectType}
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
