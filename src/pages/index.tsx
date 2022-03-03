30f86a79a359caf2fb4c2ac735ff27a5c0fdde69
Raymond Tetteh <raymondtetteh111@gmail.com>
2022-03-03 13:05:16 +0000

    address stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { formatEthAddress } from "eth-address";
 import { useMetaMask } from "metamask-react/lib/use-metamask";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
 
-import Footer from "@/components/layout/Footer";
+import Footer from "@/components/layout/footer";
 import ExploreCategories from "@/components/pages/landing/categories";
 import Header from "@/components/pages/landing/header";
 import LaunchingSoon from "@/components/pages/landing/launchingsoon";
@@ -21,7 +20,7 @@ export default function HomePage() {
   useEffect(() => {
     if (status == "connected" && account)
       axios
-        .post("/api/user", { address: formatEthAddress(account) })
+        .post("/api/user", { address: account })
         .then(() => {
           return;
         })
