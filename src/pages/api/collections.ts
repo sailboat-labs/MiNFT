b2268dff3f013943460729f3c7c8af9b3fb2b618
Raymond Tetteh <raymondtetteh111@gmail.com>
2022-03-03 16:50:02 +0000

    upload and add collection

---
+++ src/pages/api/collections.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { firebaseApp } from "@/lib/firebase";
+
+const firestore = getFirestore(firebaseApp);
+const collectionsCollection = collection(firestore, "collections");
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.method);
+  try {
+    if (req.method == "POST") {
+      const { collection } = req.body;
+
+      const _doc = doc(firestore, `collections/${collection.id}`);
+      await setDoc(_doc, collection);
+
+      return res.status(200).json({ success: true });
+    }
+    return res.status(500).json({ success: false, message: "Wrong method" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, error, message: "Unable to process request" });
+  }
+};
+
+export default handler;
--- src/types.tsx
+++ src/types.tsx
@@ -1,4 +1,28 @@
 export interface User {
+  id?: string;
+  address?: string;
+}
+
+export interface Collection {
   id: string;
-  address: string;
+  name?: string;
+  blockchain?: string;
+  projectType?: string;
+  website?: string;
+  twitter?: string;
+  discord?: string;
+  etherscan?: string;
+  opensea?: string;
+  description?: string;
+  preMintDate?: string;
+  publicMintDate?: string;
+  preSaleCost?: string;
+  publicMintCost?: string;
+  supply?: string;
+  whitelistAvailable?: string;
+  whitelistRequirements?: string;
+  teamInfo?: string;
+  image?: string;
+  dateCreated?: string;
+  lastUpdated?: string;
 }
