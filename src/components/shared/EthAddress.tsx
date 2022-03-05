741a5ab2adcae09d8f7f15dcd985ced298875fdc
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-03-05 10:27:27 +0000

    eth address component

--- src/components/shared/EthAddress.tsx
+++ src/components/shared/EthAddress.tsx
@@ -1,4 +1,5 @@
 import { formatEthAddress } from "eth-address";
+import { useState } from "react";
 
 type props = {
   account?: string;
@@ -6,5 +7,36 @@ type props = {
 };
 
 export default function EthAddress({ account,className }: props) {
-  return <>{account && <div className={className}>{formatEthAddress(account)}</div>}</>;
+  const [showCopy, setShowCopy] = useState(false)
+  return (
+    <>
+      {account && (
+        <div
+          onMouseOver={() => {
+            setShowCopy(true);
+          }}
+          onMouseLeave={() => {
+            setShowCopy(false);
+          }}
+          className={`flex cursor-pointer gap-2 rounded-xl border-2 px-5 py-1 hover:bg-gray-100 ${className}`}
+        >
+          {formatEthAddress(account)}{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-6 w-6 transition-all ${showCopy ? "translate-x-0 opacity-100" :"-translate-x-2 opacity-0 w-0"}`}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+            />
+          </svg>
+        </div>
+      )}
+    </>
+  );
 }
