2b2802ba382db7bd53c80e043051980556087de5
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-03-05 10:59:54 +0000

    profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,6 +4,8 @@ import Layout from "@/components/layout/Layout";
 import EthAddress from "@/components/shared/EthAddress";
 import { useEffect, useState } from "react";
 import AuthenticationDialog from "@/components/shared/AuthenticationDialog";
+import ProfileName from "@/components/pages/profile/NameField";
+import TimezoneSelector from "./TimezoneSelector";
 
 export default function Profile() {
   const [showAuthDialog, setShowAuthDialog] = useState(false);
@@ -35,7 +37,14 @@ export default function Profile() {
           <div className="text-2xl font-bold text-gray-900">
             Connect your wallet to view your profile
           </div>
-          <div onClick={()=>{setShowAuthDialog(true)}} className="gradient-button">Connect your wallet</div>
+          <div
+            onClick={() => {
+              setShowAuthDialog(true);
+            }}
+            className="gradient-button"
+          >
+            Connect your wallet
+          </div>
         </div>
       </Layout>
     );
@@ -46,27 +55,9 @@ export default function Profile() {
       <div className="contained mt-10">
         <div className="flex flex-col items-center justify-center">
           <div className="h-36 w-36 rounded-[50%] bg-gray-200"></div>
-          <div className="mt-5 flex translate-x-2 items-center">
-            <input
-              placeholder="Fullname"
-              className=" rounded-md  text-center text-2xl font-bold"
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-              />
-            </svg>
-          </div>
+          <ProfileName />
           {account && <EthAddress className="mt-5" account={account} />}
+          <TimezoneSelector/>
         </div>
       </div>
     </Layout>
