7d9ce21758b3abfac42df95f58f931e035a85fcd
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-03-06 10:03:15 +0000

    fixes

--- src/components/pages/collection/details/stats.tsx
+++ src/components/pages/collection/details/stats.tsx
@@ -4,29 +4,33 @@ type props = {
 };
 
 export default function CollectionStats({ className, collectionStats }: props) {
+  const formatter = Intl.NumberFormat("en", { notation: "compact" });
+  // example 1
   return (
     <>
       <div className={`flex gap-10 rounded-lg border-2 px-5 py-3 ${className}`}>
         <div className="flex flex-col items-center text-center">
           <span>Total Assets</span>
-          <span className="text-lg font-bold">{collectionStats.count}</span>
+          <span className="text-lg font-bold">
+            {formatter.format(collectionStats.count)}
+          </span>
         </div>
         <div className="flex flex-col items-center text-center">
           <span>Owners</span>
           <span className="text-lg font-bold">
-            {collectionStats.num_owners}
+            {formatter.format(collectionStats.num_owners)}
           </span>
         </div>
         <div className="flex flex-col items-center text-center">
           <span>Floor Price</span>
           <span className="text-lg font-bold">
-            {collectionStats.floor_price}
+            {formatter.format(collectionStats.floor_price)}
           </span>
         </div>
         <div className="flex flex-col items-center text-center">
           <span>Total Volume</span>
           <span className="text-lg font-bold">
-            {collectionStats.total_volume}
+            {formatter.format(collectionStats.total_volume)}
           </span>
         </div>
       </div>
