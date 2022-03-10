f4ef01b2f9956dd450bf08ba9df5ec2d70fdd10c
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-03-10 15:07:47 +0000

    FAQs added

--- src/components/pages/landing/FAQ.tsx
+++ src/components/pages/landing/FAQ.tsx
@@ -7,21 +7,31 @@ export default function FAQ() {
     { title: "What is NFT", details: "NFT is a non fungible token" },
   ];
 
-
-  const [selectedItem, setSelectedItem] = useState(-1)
+  const [selectedItem, setSelectedItem] = useState(-1);
 
   return (
     <div id="faq" data-accordion="collapse" className="contained mt-10">
       <a href="#faq" className="flex">
-        <strong className="text-xl dark:text-white">Frequently Asked Questions</strong>
+        <strong className="text-xl dark:text-white">
+          Frequently Asked Questions
+        </strong>
       </a>
-      <div className="mt-5">
+      <div className="mt-5 flex flex-col gap-3">
         {FAQ.map((item, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            onClick={() => {
+              selectedItem == index
+                ? setSelectedItem(-1)
+                : setSelectedItem(index);
+            }}
+          >
             <h2 id="accordion-collapse-heading-1">
               <button
                 type="button"
-                className="flex w-full items-center justify-between rounded-t-xl border border-b-0 border-gray-200 bg-gray-100 p-5 text-left font-medium text-gray-900 hover:bg-gray-100 focus:ring-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-800 "
+                className={`${
+                  selectedItem == index ? "rounded-t-xl" : "rounded-xl"
+                } flex w-full items-center justify-between border border-b-0 border-gray-200 bg-gray-100 p-5 text-left font-medium text-gray-900 hover:bg-gray-100 focus:ring-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-800`}
                 data-accordion-target="#accordion-collapse-body-1"
                 aria-expanded="true"
                 aria-controls="accordion-collapse-body-1"
@@ -29,7 +39,9 @@ export default function FAQ() {
                 <span className="text-sm">{item.title}</span>
                 <svg
                   data-accordion-icon=""
-                  className="h-6 w-6 shrink-0 rotate-180"
+                  className={`h-6 w-6 shrink-0 transition-all  ${
+                    selectedItem == index ? "rotate-180" : "rotate-0"
+                  }`}
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -43,11 +55,13 @@ export default function FAQ() {
               </button>
             </h2>
             <div
-              id="accordion-collapse-body-1"
-              aria-labelledby="accordion-collapse-heading-1"
-              className="h-0"
+              className={`rounded-b-xl ${
+                selectedItem == index
+                  ? "h-auto opacity-100 "
+                  : "pointer-events-none h-0 opacity-0"
+              }`}
             >
-              <div className="border border-b-0 border-gray-200 p-5 opacity-0 dark:border-gray-700 dark:bg-gray-900">
+              <div className="rounded-b-xl border border-gray-200 p-5  dark:border-gray-700 dark:bg-gray-900">
                 {item.details}
               </div>
             </div>
