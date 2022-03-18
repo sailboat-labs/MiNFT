c80e9cd8f0c93d14aebf08802aad15e47564a157
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-03-18 11:46:16 +0000

    fixes

--- src/components/pages/collection/add/MintDates.tsx
+++ src/components/pages/collection/add/MintDates.tsx
@@ -6,11 +6,14 @@ import { useEffect } from "react";
 
 import useAuthenticationDialog from "@/hooks/UseAuthDialog";
 import useUserData from "@/hooks/useUserData";
+import { useRouter } from "next/router";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function MintDates(props: any) {
   const { user, setWalletId } = useUserData();
   const { account } = useAuthenticationDialog();
+
+  const router = useRouter();
 
   useEffect(() => {
     if (!account) return;
@@ -19,11 +22,33 @@ export default function MintDates(props: any) {
 
   return (
     <div className="mt-10 flex flex-col gap-5">
-      <div className="flex items-center ">
+      <div className="flex items-start ">
         <span className="whitespace-nowrap py-2 px-6 text-sm font-medium text-gray-900 dark:text-gray-200 ">
           Your TimeZone:
         </span>
-        {user?.timeZone}
+        <div className="flex flex-col">
+          {user?.timeZone}{" "}
+          <span
+            onClick={() => {
+              router.push("/profile");
+            }}
+            className="flex items-center gap-3 cursor-pointer text-xs text-blue-500 transition-all hover:text-blue-700 dark:hover:text-blue-500 dark:text-blue-300"
+          >
+            Go to your profile to change your timezone
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </div>
       </div>
       <div className="flex items-center ">
         <span className="whitespace-nowrap py-2 px-6 text-sm font-medium text-gray-900 dark:text-gray-200 ">
