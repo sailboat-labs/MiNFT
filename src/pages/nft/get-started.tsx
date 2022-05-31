0b9fb14be26480d2428c3a8c0f027e7b73b19000
Obitrim <pevangelist516@gmail.com>
2022-05-31 08:00:06 +0000

    marked out page

--- src/pages/nft/get-started.tsx
+++ src/pages/nft/get-started.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import React from "react";
 
 import NewProperty from "@/components/nft/NewProperty";
+import NFTPreview from "@/components/nft/NFTPreview";
+import PropertyGroup from "@/components/nft/PropertyGroup";
 
 const GetStartedPage = () => {
   return (
@@ -9,9 +11,36 @@ const GetStartedPage = () => {
       <Head>
         <title>Get Started</title>
       </Head>
-      <section className="min-h-screen w-screen bg-white">
-        <div className="container mx-auto max-w-7xl px-4">
-          <NewProperty />
+      <section className="box-border flex min-h-screen bg-white">
+        <div className="container mx-auto flex max-w-7xl items-start justify-between gap-8 p-12 px-4">
+          <section className="flex-1">
+            <NewProperty />
+            {/* Group Previews */}
+            <div className="mt-10 flex flex-col gap-10">
+              <PropertyGroup />
+              <PropertyGroup />
+              <PropertyGroup />
+              <PropertyGroup />
+              <PropertyGroup />
+            </div>
+          </section>
+          <section className="max-w-[308px] flex-1">
+            {/* Project Name */}
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                placeholder="Project name"
+                className=" flex-1 rounded-sm border-[color:var(--border-gray)]"
+              />
+              <button className="flex items-center gap-2 rounded-md bg-[color:var(--blue)] py-2 px-4 text-white">
+                Save
+              </button>
+            </div>
+            {/* Project preview */}
+            <NFTPreview />
+            {/* collection size */}
+            {/* Generate collection */}
+          </section>
         </div>
       </section>
     </>
