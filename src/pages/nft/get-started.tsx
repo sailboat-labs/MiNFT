c8ca4389fcc57d3aa0e20a862f4ccf8b936222fb
Obitrim <pevangelist516@gmail.com>
2022-05-31 03:41:16 +0000

    feature: added template for adding new property

--- src/pages/nft/get-started.tsx
+++ src/pages/nft/get-started.tsx
@@ -1,7 +1,21 @@
+import Head from "next/head";
 import React from "react";
 
+import NewProperty from "@/components/nft/NewProperty";
+
 const GetStartedPage = () => {
-  return <section className="min-h-screen w-screen bg-white">Heya</section>;
+  return (
+    <>
+      <Head>
+        <title>Get Started</title>
+      </Head>
+      <section className="min-h-screen w-screen bg-white">
+        <div className="container mx-auto max-w-7xl px-4">
+          <NewProperty />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default GetStartedPage;
