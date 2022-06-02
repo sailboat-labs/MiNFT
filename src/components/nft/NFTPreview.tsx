83d56cbc704162a1c50ef94d5b492b81001786c6
Obitrim <pevangelist516@gmail.com>
2022-06-02 13:18:34 +0000

    feature: type checked values

--- src/components/nft/NFTPreview.tsx
+++ src/components/nft/NFTPreview.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from "react";
 
+import NFTPreviewInterface from "@/interfaces/get-started";
+
 interface AppProps {
   className?: string;
+  nft: NFTPreviewInterface;
 }
 
-const NFTPreview: FC<AppProps> = ({ className }) => {
+const NFTPreview: FC<AppProps> = ({ className, nft }) => {
   return (
     <div className={` h-[308px] rounded-lg bg-[#E7ECF3] ${className}`}>
       <div className="flex justify-center">
         <div className="inline-block -translate-y-4 transform rounded-md border border-[#E7ECF3] bg-[#F3F7FA] px-6 py-2 font-semibold text-indigo-700">
           Preview
         </div>
+        <div>nft</div>
       </div>
     </div>
   );
