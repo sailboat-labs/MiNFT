cb3f53439dd3e3d64de6ef32fbb5c1e4afb93ce7
Obitrim <pevangelist516@gmail.com>
2022-06-02 07:01:44 +0000

    marks out NFTPreview component

--- src/components/nft/NFTPreview.tsx
+++ src/components/nft/NFTPreview.tsx
@@ -1,13 +1,17 @@
-import React, { FC, HTMLAttributes } from "react";
+import React, { FC } from "react";
 
 interface AppProps {
-  props?: HTMLAttributes<HTMLDivElement>;
+  className?: string;
 }
 
-const NFTPreview: FC<AppProps> = ({ ...props }) => {
+const NFTPreview: FC<AppProps> = ({ className }) => {
   return (
-    <div className="bg-[color:var(--bg-indigo)]" {...props}>
-      <div>Preview</div>
+    <div className={` h-[308px] rounded-lg bg-[#E7ECF3] ${className}`}>
+      <div className="flex justify-center">
+        <div className="inline-block -translate-y-4 transform rounded-md border border-[#E7ECF3] bg-[#F3F7FA] px-6 py-2 font-semibold text-indigo-700">
+          Preview
+        </div>
+      </div>
     </div>
   );
 };
