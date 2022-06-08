087aecc795511b39deccc9e8e37f5f15e6484b23
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-06-08 15:14:20 +0000

    Upload Element

--- src/components/nft/PropertyGroup.tsx
+++ src/components/nft/PropertyGroup.tsx
@@ -3,6 +3,7 @@ import React, { FC } from "react";
 import { IElement } from "@/interfaces/get-started";
 
 import TraitPreview from "./TraitPreview";
+import UploadElement from "./UploadElement";
 
 interface AppProps {
   name: string;
@@ -53,36 +54,10 @@ const PropertyGroup: FC<AppProps> = ({ name, onChange }) => {
           />
         ))} */}
         <div className="flex h-[76px] items-center gap-3">
-          <button className="grid h-12 w-12 place-content-center rounded-full bg-white shadow-md">
-            <svg
-              xmlns="http://www.w3.org/1200/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
-          </button>
-          <button className="grid h-12 w-12 place-content-center rounded-full bg-white shadow-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+          <UploadElement
+            layerName={name}
+            
+          />
         </div>
       </div>
     </div>
