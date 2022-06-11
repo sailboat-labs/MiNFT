d7e87f7b84e659762e7a98fd4344de0a281d60e4
Obitrim <pevangelist516@gmail.com>
2022-06-11 13:21:49 +0000

    feature: added switch component to list

--- src/components/SortableList/SortableItem.tsx
+++ src/components/SortableList/SortableItem.tsx
@@ -1,24 +1,50 @@
+import { Switch } from "@headlessui/react";
 import React from "react";
 import { SortableElement } from "react-sortable-hoc";
 
-const SortableItem = SortableElement(({ value }: { value: string }) => (
-  <li className="flex cursor-grab list-none items-center gap-2 py-4">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-4"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-      />
-    </svg>
-    <span> {value}</span>
-  </li>
-));
+interface AppProps {
+  id: number;
+  value: string;
+  enabled: boolean;
+  toggleTrait: (index: number) => void;
+}
+
+const SortableItem = SortableElement(
+  ({ value, enabled, toggleTrait, id }: AppProps) => (
+    <li className="flex cursor-grab list-none items-center justify-between gap-2 py-4 pr-4">
+      <div className="flex gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+          />
+        </svg>
+        <span> {value}</span>
+      </div>
+      <Switch
+        checked={enabled}
+        onChange={() => toggleTrait(id)}
+        className={`${
+          enabled ? "bg-blue-600" : "bg-gray-200"
+        } relative inline-flex h-6 w-11 items-center rounded-full`}
+      >
+        <span className="sr-only">Enable notifications</span>
+        <span
+          className={`${
+            enabled ? "translate-x-6" : "translate-x-1"
+          } pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white transition-all duration-100`}
+        />
+      </Switch>
+    </li>
+  )
+);
 
 export default SortableItem;
