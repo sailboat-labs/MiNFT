0d43e38224446646f8ad4c2157a4e2049424425c
Obitrim <pevangelist516@gmail.com>
2022-06-11 13:21:30 +0000

    feature: improved reusability of component

--- src/components/SortableList/index.tsx
+++ src/components/SortableList/index.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { SortableContainer } from "react-sortable-hoc";
 
+import { Trait } from "@/interfaces/get-started";
+
 import SortableItem from "./SortableItem";
 
-const SortableList = SortableContainer(({ items }: { items: string[] }) => {
+interface AppProps {
+  items: Trait[];
+  setTraits: (traits: Trait[]) => void;
+}
+
+const SortableList = SortableContainer(({ items, setTraits }: AppProps) => {
+  /**
+   * toggles a specific trait
+   *
+   * @param {number} id - id of trait
+   * @returns {undefined}
+   */
+  function toggleTrait(id: number): void {
+    const temp = [...items];
+    const traitIndex = temp.findIndex((item) => item.id === id);
+    temp[traitIndex] = {
+      ...temp[traitIndex],
+      enabled: !temp[traitIndex].enabled,
+    };
+
+    setTraits(temp);
+  }
+
   return (
     <ul>
-      {items.map((item: string, index: number) => (
-        <SortableItem key={item + index} index={index} value={item} />
+      {items.map((item: Trait, index: number) => (
+        <SortableItem
+          enabled={item.enabled}
+          toggleTrait={toggleTrait}
+          key={item.id}
+          id={item.id}
+          index={index}
+          value={item.name}
+        />
       ))}
     </ul>
   );
