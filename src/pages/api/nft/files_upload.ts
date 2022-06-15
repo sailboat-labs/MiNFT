126b7fdf19deb4269c019a5ddff527a80f98f365
Obitrim <pevangelist516@gmail.com>
2022-06-15 07:22:02 +0000

    feature: handles file upload to server

---
+++ src/pages/api/nft/files_upload.ts
@@ -0,0 +1,43 @@
+import fs from "fs";
+import multer from "multer";
+import nc from "next-connect";
+
+const storage = multer.diskStorage({
+  destination: function (req: any, file: any, cb: any) {
+    const baseDir = `./public/art-engine/images/`;
+
+    const accountDir = baseDir.concat(req.query.account);
+    if (!fs.existsSync(accountDir)) {
+      fs.mkdirSync(accountDir);
+    }
+    const layerName = file.originalname.split("/")[1];
+    const fileUploadDir = accountDir.concat("/").concat(layerName);
+    if (!fs.existsSync(fileUploadDir)) {
+      fs.mkdirSync(fileUploadDir);
+    }
+
+    cb(null, fileUploadDir);
+  },
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, file.originalname.split("/")[2]);
+  },
+});
+const uploader = multer({ storage, preservePath: true });
+
+const handler = nc<any, any>();
+handler.use(uploader.array("files"));
+
+handler.post((req, res) => {
+  res.status(200).json({
+    files: req.files,
+    data: req.body,
+  });
+});
+
+export default handler;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
