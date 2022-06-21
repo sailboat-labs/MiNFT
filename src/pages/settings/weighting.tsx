e6148dfc22f15f84dd67983f45080f3c5966d18b
Obitrim <pevangelist516@gmail.com>
2022-06-21 07:01:06 +0000

    feature: refactored page content

--- src/pages/settings/weighting.tsx
+++ src/pages/settings/weighting.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { ReactElement } from "react";
+
+import SettingsLayout from "@/components/layout/Settings";
 
 const WeightingSettingsPage = () => {
-  return <div>Weighting Settings</div>;
+  return (
+    <section className="flex-1 overflow-y-auto bg-gray-100">
+      <div className="container mx-auto max-w-2xl flex-1 divide-y divide-gray-200 bg-gray-100 py-10">
+        Weighting Settings
+      </div>
+    </section>
+  );
+};
+
+WeightingSettingsPage.getLayout = function getLayout(page: ReactElement) {
+  return <SettingsLayout>{page}</SettingsLayout>;
 };
 
 export default WeightingSettingsPage;
