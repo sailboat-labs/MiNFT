49f663fb21c0159bf5e073fd517c9bb10577a9f3
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-06-28 12:44:24 +0000

    Trait preview accordion height resize

--- src/components/nft/PropertyGroup.tsx
+++ src/components/nft/PropertyGroup.tsx
@@ -269,41 +269,43 @@ const PropertyGroup: FC<AppProps> = ({
           ref={accordionContent}
         >
           <div className="rounded-md border-2 bg-gray-100">
-            <div
-              className={`flex flex-wrap gap-6 p-6 transition-all ${
-                elements.length < 1 ? "mt-0" : "mt-0"
-              }`}
-            >
-              {elements.map((element: IElement, index: number) => (
-                <TraitPreview
-                  key={index}
-                  file={element}
-                  traitIndex={index}
-                  rarityMode={layer.name === selectedLayerName}
-                  onRemove={removeTrait}
-                  active={element.isSelected}
-                />
-              ))}
+            <div>
+              <div
+                className={`flex flex-wrap gap-6 p-6 transition-all ${
+                  elements.length < 1 ? "mt-0" : "mt-0"
+                }`}
+              >
+                {elements.map((element: IElement, index: number) => (
+                  <TraitPreview
+                    key={index}
+                    file={element}
+                    traitIndex={index}
+                    rarityMode={layer.name === selectedLayerName}
+                    onRemove={removeTrait}
+                    active={element.isSelected}
+                  />
+                ))}
 
-              {elements.length < 1 && (
-                <div className="flex items-center gap-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-10 w-10 rounded-full border-2 border-red-500 bg-red-200 p-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="red"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                    />
-                  </svg>
-                  No element added
-                </div>
-              )}
+                {elements.length < 1 && (
+                  <div className="flex items-center gap-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-10 w-10 rounded-full border-2 border-red-500 bg-red-200 p-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="red"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                      />
+                    </svg>
+                    No element added
+                  </div>
+                )}
+              </div>
             </div>
             {changingRarity() && (
               <div className="mb-4  flex items-center justify-center gap-x-4 px-6">
