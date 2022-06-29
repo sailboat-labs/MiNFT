544d5aafa21259d16dc7364455943c79a89e282a
Obitrim <pevangelist516@gmail.com>
2022-06-29 11:58:18 +0000

    fix: clipped overflow scroll bug

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
         </Head>
-        <body>
+        <body className="h-screen overflow-hidden">
           <Main />
           <NextScript />
         </body>
