0128bde00968e5796aa8c958ecd54a4d1c291659
Obitrim <pevangelist516@gmail.com>
2022-06-29 11:59:23 +0000

    feature: hides back and next buttons when active step is less than 1 and greater than maximum number of steps respectively

--- src/pages/dashboard/contract-maker.tsx
+++ src/pages/dashboard/contract-maker.tsx
@@ -20,21 +20,25 @@ const ContractMakerView = () => {
   return (
     <main className="flex-1">
       {/* <Stepper stepsCount={6} activeStep={3} /> */}
-      <div className="rounded-md bg-indigo-100 p-6">
+      <div className="my-10 rounded-md bg-indigo-50 p-6">
         {STEPS[activeStep]}
-        <section className="flex items-center justify-center gap-3">
-          <button
-            onClick={() => nextStep(-1)}
-            className="min-w-[150px] rounded-md bg-indigo-800 py-2 text-white transition-all duration-150 hover:bg-indigo-900"
-          >
-            Back
-          </button>
-          <button
-            onClick={() => nextStep()}
-            className="min-w-[150px] rounded-md bg-indigo-800 py-2 text-white transition-all duration-150 hover:bg-indigo-900"
-          >
-            Next
-          </button>
+        <section className="flex items-center justify-center gap-3 pt-16">
+          {activeStep > 1 && (
+            <button
+              onClick={() => nextStep(-1)}
+              className="min-w-[150px] rounded-md bg-indigo-800 py-2 text-white transition-all duration-150 hover:bg-indigo-900"
+            >
+              Back
+            </button>
+          )}
+          {activeStep < 3 && (
+            <button
+              onClick={() => nextStep()}
+              className="min-w-[150px] rounded-md bg-indigo-800 py-2 text-white transition-all duration-150 hover:bg-indigo-900"
+            >
+              Next
+            </button>
+          )}
         </section>
       </div>
     </main>
