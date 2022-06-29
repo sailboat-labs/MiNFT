e4fe22af9644607dcbdbeab58337cd6d353ef510
Obitrim <pevangelist516@gmail.com>
2022-06-29 08:26:19 +0000

    feature: added components to page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,12 +6,15 @@ import NFTGenerator from "@/components/pages/Dashboard/NftGenerator";
 
 import { IDashboardState } from "@/interfaces";
 
+import ContractMakerView from "./contract-maker";
+
 export default function DashboardHomePage() {
   const dashboardState = useSelector(getDashboardState) as IDashboardState;
   const selectedSidebar = dashboardState.selectedSidebar;
 
   const content: { component: any; value: string }[] = [
     { component: <NFTGenerator />, value: "nft-generator" },
+    { component: <ContractMakerView />, value: "contract-maker" },
   ];
 
   return (
