4dda4e3ea1d9ebb4c3e49ea30394adf8bb96b629
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-06-30 07:34:48 +0000

    fixes

--- src/components/nft/GenerateToken.tsx
+++ src/components/nft/GenerateToken.tsx
@@ -1003,7 +1003,7 @@ export default function GenerateToken() {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 ml-72 overflow-y-auto">
+          <div className="fixed inset-0 ml-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center text-center">
               <Transition.Child
                 as={Fragment}
