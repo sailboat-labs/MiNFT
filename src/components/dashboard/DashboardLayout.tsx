966d3e3f0575272e49ac9d3068353b7c6c1ee26f
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-07-04 12:19:24 +0000

    Authentication

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getSlideInModalState } from "redux/reducers/selectors/dashboard";
 import { setSlideInModalConfig } from "redux/reducers/slices/dashboard";
 
+import useAuthenticationDialog from "@/hooks/UseAuthDialog";
+
 import Navbar from "./NavBar";
 import Sidebar from "./Sidebar";
+import Layout from "../layout/Layout";
 import SlideInModal from "../modals/SlideIn";
 import BasicSettings from "../pages/settings/BasicSettings";
 import CollectionSettings from "../pages/settings/Collection";
@@ -25,6 +28,7 @@ export default function DashboardLayout({
 }: props) {
   const slideInModalState = useSelector(getSlideInModalState);
   const dispatch = useDispatch();
+  const [profile, setProfile] = useState<any>();
 
   const slidInComponentMap: { name: string; component: any }[] = [
     {
@@ -38,6 +42,51 @@ export default function DashboardLayout({
       ),
     },
   ];
+
+  // const { account, chainId, isAuthenticated } = useMoralis();
+  const { AuthDialog, setShowAuthDialog, account, isAuthenticated } =
+    useAuthenticationDialog();
+
+  useEffect(() => {
+    if (!account || !isAuthenticated) return;
+
+    // setProfile(user);
+  }, [account, isAuthenticated]);
+
+  if (!account || !isAuthenticated) {
+    return (
+      <Layout>
+        <div className="mt-20 flex h-full w-full flex-col items-center justify-center gap-5 px-10 text-center">
+          <AuthDialog />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-20 w-20"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <div className="text-2xl font-bold text-gray-900 dark:text-white">
+            Connect your wallet to view your profile
+          </div>
+          <div
+            onClick={() => {
+              setShowAuthDialog(true);
+            }}
+            className="gradient-button"
+          >
+            Connect your wallet
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 
   return (
     <main>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 
 import Footer from "./footer";
-import Navbar from "./Navbar";
 import Seo from "../Seo";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col bg-white text-black dark:bg-black dark:text-white">
       <Seo />
 
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="min-h-screen pt-[3.2rem]">
         <div>{children}</div>
       </div>
