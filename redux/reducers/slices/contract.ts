c2b1b813f4f5a1532fbce65fb4059a9d1038d9a5
Obitrim <pevangelist516@gmail.com>
2022-07-05 12:08:33 +0000

    feat: added field to update whitelist details

--- redux/reducers/slices/contract.ts
+++ redux/reducers/slices/contract.ts
@@ -1,18 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const contractSlice = createSlice({
   name: "contract",
   initialState: {
     type: "Classic Mint",
-    whitelist: false,
+    whitelisted: false,
+    whitelistDetails: null,
   },
   reducers: {
-    setContract: (state: any, action: any) => {
+    setContract: (
+      state: any,
+      action: PayloadAction<{ key: string; value: any }>
+    ) => {
       const { key, value } = action.payload;
       state[key] = value;
+    },
+    updateWhitelistDetails: (state: any, action: PayloadAction<any>) => {
+      state.whitelistDetails = state.whitelistDetails
+        ? { ...state.whitelistDetails, ...action.payload }
+        : action.payload;
     },
   },
 });
 const { actions } = contractSlice;
-export const { setContract } = actions;
+export const { setContract, updateWhitelistDetails } = actions;
 export default contractSlice;
