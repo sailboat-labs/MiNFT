bd8dd781156d29695f1bb9737bddf37c1c713f44
Obitrim <pevangelist516@gmail.com>
2022-07-09 10:27:59 +0000

    feat: makes graph dynamic

--- src/components/contract/ContractSettingsStep.tsx
+++ src/components/contract/ContractSettingsStep.tsx
@@ -19,6 +19,32 @@ const TEMPLATES: {
   "Fair Dutch Auction": <FairDutchAuction />,
 };
 
+const GRAPHS: any = {
+  "classic mint": "/svg/classic-mint-graph.svg",
+  "pure whitelist": "/svg/classic-mint-graph.svg",
+  "dutch auction": "/svg/dutch-graph.svg",
+  "fair dutch auction": "/svg/dutch-graph.svg",
+};
+
+const GAS_FEES: any = {
+  "classic mint": {
+    from: "Medium",
+    to: "High",
+  },
+  "dutch auction": {
+    from: "Low",
+    to: "Medium",
+  },
+  "fair dutch auction": {
+    from: "Low",
+    to: "Medium",
+  },
+  "pure whitelist": {
+    from: "Low",
+    to: null,
+  },
+};
+
 interface AppProps {
   isPreview?: boolean;
 }
@@ -40,16 +66,18 @@ const ContractSettingsStep: FC<AppProps> = ({ isPreview = false }) => {
     }
   }, [isPreview]);
 
+  const gasFee = GAS_FEES[type.trim().toLowerCase()];
+
   return (
     <section className="mx-auto max-w-4xl">
       {isPreview ? (
         <>
           <article className="-mb-4 flex gap-8 pt-10 pb-8">
             <div className="w-[200px]">
-              <img src="/svg/dutch-graph.svg" alt="image" />
+              <img src={GRAPHS[type.trim().toLowerCase()]} alt="" />
             </div>
             <div className="flex-1">
-              <strong className="text-sm text-indigo-800">Contract Type</strong>
+              <strong className="text-sm text-indigo-800">{type}</strong>
               <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
                 et. Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -57,22 +85,27 @@ const ContractSettingsStep: FC<AppProps> = ({ isPreview = false }) => {
               <br />
               <strong className="text-sm text-indigo-800">Gas Fees</strong>
               <p className="flex items-center gap-2">
-                <span>High</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-                <span>Low</span>
+                <span>{gasFee.from}</span>
+
+                {gasFee.to && (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                    <span>{gasFee.to}</span>
+                  </>
+                )}
               </p>
             </div>
           </article>
