740fd6802cf270aafa4ff174b970c6d2ac9cd8e8
Obitrim <pevangelist516@gmail.com>
2022-07-09 22:04:19 +0000

    feat: adds input validations

--- src/components/contract/templates/DutchAuction.tsx
+++ src/components/contract/templates/DutchAuction.tsx
@@ -1,22 +1,90 @@
+import { useFormik } from "formik";
 import React from "react";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
 
 import BaseInput from "@/components/controls/BaseInput";
 import BaseSelect from "@/components/controls/BaseSelect";
 import DateTimeRangePicker from "@/components/controls/DateTimeRangePicker";
 
 const DutchAuction = () => {
+  const dispatch = useDispatch();
+
+  const dutchAuctionForm = useFormik({
+    initialValues: {
+      quantity: 0,
+      startingPrice: 0,
+      endingPrice: 0,
+      decrementAmount: 0,
+    },
+    validationSchema: Yup.object({
+      quantity: Yup.number()
+        .required("enter collection quantity")
+        .positive("expected positive value"),
+      startingPrice: Yup.number()
+        .required("enter starting price")
+        .min(0, "starting price is required"),
+      endingPrice: Yup.number()
+        .required("ending price is required")
+        .min(0, "expected positive value"),
+      decrementAmount: Yup.number()
+        .required("enter collection quantity")
+        .positive("expected positive value"),
+    }),
+    onSubmit: (values, formik) => {
+      console.log(values);
+    },
+  });
+
+  // function handleOnChange(key: string, value: string, float = false) {
+  //   value = value.trim();
+
+  //   dispatch(
+  //     updateDutchAuction({
+  //       key,
+  //       value:
+  //         value.length === 0
+  //           ? undefined
+  //           : float
+  //           ? parseFloat(value)
+  //           : parseInt(value),
+  //     })
+  //   );
+  // }
   return (
     <div className="pt-8 pb-10">
       {/* <h2 className="text-xl text-indigo-800">Dutch auction</h2> */}
       <section className="mt-6 grid grid-cols-4 gap-x-10">
         <div className="flex flex-col">
           <strong>Quantity of collection</strong>
-          <BaseInput wrapperClass="mt-2" />
+          <BaseInput
+            type="number"
+            {...dutchAuctionForm.getFieldProps("quantity")}
+            error={
+              dutchAuctionForm.touched.quantity &&
+              dutchAuctionForm.errors.quantity ? (
+                <p className="text-base text-red-500">
+                  {dutchAuctionForm.errors.quantity}
+                </p>
+              ) : null
+            }
+            wrapperClass="mt-2"
+          />
         </div>
 
         <div className="flex flex-col">
           <strong>Starting Price</strong>
           <BaseInput
+            type="number"
+            {...dutchAuctionForm.getFieldProps("startingPrice")}
+            error={
+              dutchAuctionForm.touched.startingPrice &&
+              dutchAuctionForm.errors.startingPrice ? (
+                <p className="text-base text-red-500">
+                  {dutchAuctionForm.errors.startingPrice}
+                </p>
+              ) : null
+            }
             wrapperClass="mt-2"
             placeholder="starting price"
             postfix={<span className="font-semibold text-indigo-800">ETH</span>}
@@ -25,6 +93,15 @@ const DutchAuction = () => {
         <div className="flex flex-col">
           <strong>Ending Price</strong>
           <BaseInput
+            {...dutchAuctionForm.getFieldProps("endingPrice")}
+            error={
+              dutchAuctionForm.touched.endingPrice &&
+              dutchAuctionForm.errors.endingPrice ? (
+                <p className="text-base text-red-500">
+                  {dutchAuctionForm.errors.endingPrice}
+                </p>
+              ) : null
+            }
             wrapperClass="mt-2"
             placeholder="ending price"
             postfix={<span className="font-semibold text-indigo-800">ETH</span>}
@@ -33,6 +110,16 @@ const DutchAuction = () => {
         <div className="flex flex-col">
           <strong>Amount Decrement</strong>
           <BaseInput
+            type="number"
+            {...dutchAuctionForm.getFieldProps("decrementAmount")}
+            error={
+              dutchAuctionForm.touched.decrementAmount &&
+              dutchAuctionForm.errors.decrementAmount ? (
+                <p className="text-base text-red-500">
+                  {dutchAuctionForm.errors.decrementAmount}
+                </p>
+              ) : null
+            }
             wrapperClass="mt-2"
             placeholder="amount"
             postfix={<span className="font-semibold text-indigo-800">ETH</span>}
