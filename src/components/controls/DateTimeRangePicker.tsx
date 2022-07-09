2c1a0d272bd43eb8a0504e7eef93fb1d87f601db
Obitrim <pevangelist516@gmail.com>
2022-07-09 22:23:30 +0000

    refactor: removed code

--- src/components/controls/DateTimeRangePicker.tsx
+++ src/components/controls/DateTimeRangePicker.tsx
@@ -1,26 +1,8 @@
 import React from "react";
 
-import { getYearsBackDated } from "@/utils/date";
-
 import BaseDatePicker from "./BaseDatePicker";
 
 const DateTimeRangePicker = () => {
-  const MONTHS: string[] = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const YEARS: number[] = getYearsBackDated(15);
-
   return (
     <section className="gap-10">
       <div className="box-content grid w-fit grid-cols-2 gap-x-10 rounded-md bg-white p-5 ring-1 ring-gray-200">
