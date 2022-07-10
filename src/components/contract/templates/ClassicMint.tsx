0f7af293fa82fc5f0053f0ed0a8830cfb356dcab
Obitrim <pevangelist516@gmail.com>
2022-07-10 18:00:10 +0000

    refactor: extracted template

--- src/components/contract/templates/ClassicMint.tsx
+++ src/components/contract/templates/ClassicMint.tsx
@@ -1,17 +1,10 @@
 import { useFormik } from "formik";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { updateClassicMint } from "redux/reducers/slices/contract";
 import * as Yup from "yup";
 
-import BaseInput from "@/components/controls/BaseInput";
-import BaseSelect from "@/components/controls/BaseSelect";
-import DateTimeRangePicker from "@/components/controls/DateTimeRangePicker";
-import ContractFormRowSection from "@/components/layout/ContractRowSection";
+import ClassicMintFormFields from "../ClassicMintFormFields";
 
 const ClassicMint = () => {
-  const dispatch = useDispatch();
-
   const classicMintForm = useFormik({
     initialValues: {
       quantityOfCollection: 0,
@@ -46,128 +39,7 @@ const ClassicMint = () => {
     },
   });
 
-  function handleOnChange(key: string, value: string) {
-    value = value.trim();
-
-    dispatch(
-      updateClassicMint({
-        key,
-        value: value.length === 0 ? undefined : parseInt(value),
-      })
-    );
-  }
-
-  return (
-    <div className="py-8">
-      <section className="mt-6 flex flex-col gap-y-4">
-        <ContractFormRowSection
-          headingClass="font-semibold text-base"
-          name="Quantity of collection"
-        >
-          <BaseInput
-            {...classicMintForm.getFieldProps("quantityOfCollection")}
-            type="number"
-            wrapperClass="w-full"
-            placeholder="Quantity"
-            error={
-              classicMintForm.touched.quantityOfCollection &&
-              classicMintForm.errors.quantityOfCollection ? (
-                <div>{classicMintForm.errors.quantityOfCollection}</div>
-              ) : null
-            }
-          />
-        </ContractFormRowSection>
-        <ContractFormRowSection
-          headingClass="font-semibold text-base"
-          name="Mint Price"
-        >
-          <BaseInput
-            wrapperClass="w-full"
-            type="number"
-            placeholder="Price"
-            {...classicMintForm.getFieldProps("mintPrice")}
-            error={
-              classicMintForm.touched.mintPrice &&
-              classicMintForm.errors.mintPrice ? (
-                <p className="text-base text-red-500">
-                  {classicMintForm.errors.mintPrice}
-                </p>
-              ) : null
-            }
-            postfix={<span className="font-semibold text-indigo-800">ETH</span>}
-          />
-        </ContractFormRowSection>
-        <ContractFormRowSection
-          headingClass="font-semibold text-base"
-          name="Number of tokens to reserve"
-        >
-          <BaseInput
-            type="number"
-            {...classicMintForm.getFieldProps("reservedTokens")}
-            error={
-              classicMintForm.touched.reservedTokens &&
-              classicMintForm.errors.reservedTokens ? (
-                <p className="text-base text-red-500">
-                  {classicMintForm.errors.reservedTokens}
-                </p>
-              ) : null
-            }
-            wrapperClass="w-full"
-          />
-        </ContractFormRowSection>
-        <ContractFormRowSection
-          headingClass="font-semibold text-base"
-          name="Max mint per wallet"
-        >
-          <BaseInput
-            {...classicMintForm.getFieldProps("maxMintPerWallet")}
-            error={
-              classicMintForm.touched.maxMintPerWallet &&
-              classicMintForm.errors.maxMintPerWallet ? (
-                <p className="text-base text-red-500">
-                  {classicMintForm.errors.maxMintPerWallet}
-                </p>
-              ) : null
-            }
-            type="number"
-            wrapperClass="w-full"
-          />
-        </ContractFormRowSection>
-        <ContractFormRowSection
-          headingClass="font-semibold text-base"
-          name="Max mint per transaction"
-        >
-          <BaseInput
-            {...classicMintForm.getFieldProps("maxMintPerTransaction")}
-            error={
-              classicMintForm.touched.maxMintPerTransaction &&
-              classicMintForm.errors.maxMintPerTransaction ? (
-                <p className="text-base text-red-500">
-                  {classicMintForm.errors.maxMintPerTransaction}
-                </p>
-              ) : null
-            }
-            type="number"
-            wrapperClass="w-full"
-          />
-        </ContractFormRowSection>
-      </section>
-      <h4 className="mt-10">Timing</h4>
-      <section className="mb-10 mt-3 flex items-start  gap-10">
-        <div className="flex-1">
-          <DateTimeRangePicker />
-        </div>
-        <div className="max-w-[250px] flex-1 rounded-md bg-white p-5 ring-1 ring-gray-200">
-          <strong>Minutes</strong>
-          <BaseSelect
-            options={[]}
-            buttonClass="!bg-white ring-1 mt-1 ring-gray-200 !text-gray-800"
-            selectorIconColor="black"
-          />
-        </div>
-      </section>
-    </div>
-  );
+  return <ClassicMintFormFields form={classicMintForm} />;
 };
 
 export default ClassicMint;
--- src/components/contract/templates/DutchAuction.tsx
+++ src/components/contract/templates/DutchAuction.tsx
@@ -3,9 +3,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import * as Yup from "yup";
 
-import BaseInput from "@/components/controls/BaseInput";
-import BaseSelect from "@/components/controls/BaseSelect";
-import DateTimeRangePicker from "@/components/controls/DateTimeRangePicker";
+import DutchAuctionFormFields from "../DutchFormFields";
 
 const DutchAuction = () => {
   const dispatch = useDispatch();
@@ -51,94 +49,7 @@ const DutchAuction = () => {
   //     })
   //   );
   // }
-  return (
-    <div className="pt-8 pb-10">
-      {/* <h2 className="text-xl text-indigo-800">Dutch auction</h2> */}
-      <section className="mt-6 grid grid-cols-4 gap-x-10">
-        <div className="flex flex-col">
-          <strong>Quantity of collection</strong>
-          <BaseInput
-            type="number"
-            {...dutchAuctionForm.getFieldProps("quantity")}
-            error={
-              dutchAuctionForm.touched.quantity &&
-              dutchAuctionForm.errors.quantity ? (
-                <p className="text-base text-red-500">
-                  {dutchAuctionForm.errors.quantity}
-                </p>
-              ) : null
-            }
-            wrapperClass="mt-2"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <strong>Starting Price</strong>
-          <BaseInput
-            type="number"
-            {...dutchAuctionForm.getFieldProps("startingPrice")}
-            error={
-              dutchAuctionForm.touched.startingPrice &&
-              dutchAuctionForm.errors.startingPrice ? (
-                <p className="text-base text-red-500">
-                  {dutchAuctionForm.errors.startingPrice}
-                </p>
-              ) : null
-            }
-            wrapperClass="mt-2"
-            placeholder="starting price"
-            postfix={<span className="font-semibold text-indigo-800">ETH</span>}
-          />
-        </div>
-        <div className="flex flex-col">
-          <strong>Ending Price</strong>
-          <BaseInput
-            {...dutchAuctionForm.getFieldProps("endingPrice")}
-            error={
-              dutchAuctionForm.touched.endingPrice &&
-              dutchAuctionForm.errors.endingPrice ? (
-                <p className="text-base text-red-500">
-                  {dutchAuctionForm.errors.endingPrice}
-                </p>
-              ) : null
-            }
-            wrapperClass="mt-2"
-            placeholder="ending price"
-            postfix={<span className="font-semibold text-indigo-800">ETH</span>}
-          />
-        </div>
-        <div className="flex flex-col">
-          <strong>Amount Decrement</strong>
-          <BaseInput
-            type="number"
-            {...dutchAuctionForm.getFieldProps("decrementAmount")}
-            error={
-              dutchAuctionForm.touched.decrementAmount &&
-              dutchAuctionForm.errors.decrementAmount ? (
-                <p className="text-base text-red-500">
-                  {dutchAuctionForm.errors.decrementAmount}
-                </p>
-              ) : null
-            }
-            wrapperClass="mt-2"
-            placeholder="amount"
-            postfix={<span className="font-semibold text-indigo-800">ETH</span>}
-          />
-        </div>
-      </section>
-      <section className="mt-10 flex items-start  gap-10">
-        <DateTimeRangePicker />
-        <div className=" max-w-[300px] flex-1 rounded-md bg-white p-5 ring-1 ring-gray-200">
-          <strong>Minutes</strong>
-          <BaseSelect
-            options={[]}
-            buttonClass="!bg-white ring-1 ring-gray-200 !text-gray-800 mt-2"
-            selectorIconColor="black"
-          />
-        </div>
-      </section>
-    </div>
-  );
+  return <DutchAuctionFormFields form={dutchAuctionForm} />;
 };
 
 export default DutchAuction;
