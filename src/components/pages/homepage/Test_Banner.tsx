31e631aa9d0f6ffcd300793cdd0f1a1f8d3d97ca
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-07-11 18:12:54 +0000

    Fix spelling error

--- src/components/pages/homepage/Test_Banner.tsx
+++ src/components/pages/homepage/Test_Banner.tsx
@@ -1,10 +1,12 @@
+// ! BANNER WITH CAROUSEL / SLIDE SHOW OF INFORMATION
+
 import ButtonLink from "@/components/links/ButtonLink";
 
 export default function Test_Banner() {
   return (
     <>
-      <div className="flex h-[60vh] flex-col font-dmsans  text-white">
-        <div className="flex items-center justify-end  gap-5 p-5">
+      <div className="flex flex-col font-dmsans text-white">
+        <div className="flex items-center justify-end gap-5 p-5">
           <ButtonLink href="">Contact Us</ButtonLink>
         </div>
       </div>
