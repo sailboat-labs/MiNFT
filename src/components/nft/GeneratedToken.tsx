b74ee2a4aec88af80ee4718a9f8aeea5b54e05a7
Obitrim <pevangelist516@gmail.com>
2022-07-13 20:35:39 +0000

    style: increase stacking order

--- src/components/nft/GeneratedToken.tsx
+++ src/components/nft/GeneratedToken.tsx
@@ -52,7 +52,7 @@ export default function GeneratedToken({ token }: props) {
       </div>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-[100000]" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
