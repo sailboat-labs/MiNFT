b46f603e1f7ba7ae2251d236a255cd25bdf5c6b1
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2022-07-19 10:44:19 +0000

    Fixed responsiveness for homepage

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -10,14 +10,14 @@ export default function Header() {
 
   return (
     <div className="fixed z-40 flex h-12 w-full flex-row items-center border border-[#1F1A17] bg-white font-montserrat font-extrabold text-[#1F1A17]">
-      <StarSVG className="ml-10 mr-3 h-6 w-6 sm:ml-20 lg:ml-40" />
-      <UnstyledLink href="/" className="w-fit text-xl lg:mr-14">
+      <StarSVG className="mx-3 h-6 w-6 sm:ml-20 lg:ml-40" />
+      <UnstyledLink href="/" className="w-fit text-xl ">
         Magic Mynt
       </UnstyledLink>
-      <span className="hidden sm:w-1/5 md:inline md:w-2/5 lg:w-3/5"></span>
-      <div className=" hidden flex-row text-base md:flex lg:ml-0">
+      <span className="w-6 sm:w-1/5 md:inline md:w-2/5 lg:w-1/2 xl:w-3/5"></span>
+      <div className="hidden flex-row text-base md:flex lg:ml-0">
         <span
-          className=" mr-10 w-24 cursor-pointer lg:mr-14"
+          className=" cursor-pointer sm:mr-10 lg:mr-14"
           onClick={() => {
             process.env.NEXT_PUBLIC_ENVIRONMENT == "development"
               ? router.push("/dashboard")
