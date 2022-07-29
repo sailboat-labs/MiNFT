6fada22d21a09d75b68117ea04789c0e441a2d66
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-07-29 10:12:46 +0000

    fixes

--- features/launch/components/contact-us.tsx
+++ features/launch/components/contact-us.tsx
@@ -1,6 +1,5 @@
 import emailjs from "@emailjs/browser";
 import { Dialog, Transition } from "@headlessui/react";
-import { Input } from "@mui/material";
 import { Formik } from "formik";
 import { Fragment, useRef, useState } from "react";
 import ReCaptcha from "react-google-recaptcha";
@@ -62,7 +61,7 @@ export default function ContactUs() {
         `Hi ${e.target.name.value}, thank you for contacting Magic Mynt. We will get back to you as soon as possible.`
       );
     }
-    document.getElementById("contact-us")?.click();
+    closeModal();
   };
 
   return (
@@ -96,75 +95,65 @@ export default function ContactUs() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="h-96 w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <section className="fixed inset-0 z-[9999] flex items-center justify-center">
-                    <div
-                      id="contact-us"
-                      className="absolute inset-0 bg-[rgba(0,0,0,0.7)]"
-                      onClick={closeModal}
-                    ></div>
-                    <article className="relative z-[9999] mx-4 flex max-w-lg flex-1 flex-col overflow-hidden rounded-lg bg-white text-left md:max-w-3xl lg:max-w-lg lg:flex-row">
-                      <div className="flex w-full flex-col justify-center px-5 py-10 text-black sm:w-4/5 md:w-3/4 md:p-10 lg:w-3/5">
-                        <h4 className="mx-auto w-full px-3 text-3xl font-bold md:px-0">
-                          Contact us
-                        </h4>
-                        <div className="mx-auto flex items-center justify-center md:block lg:flex">
-                          <Formik
-                            initialValues={{
-                              name: "",
-                              email: "",
-                              message: "",
-                            }}
-                            validationSchema={validate}
-                            onSubmit={() => {
-                              //
-                            }}
+                <Dialog.Panel className="flex h-fit w-fit transform flex-col overflow-hidden rounded-2xl bg-white p-6 pr-20 text-left align-middle shadow-xl transition-all">
+                  <h4 className="mx-auto w-full px-3 text-3xl font-bold md:px-0">
+                    Contact us
+                  </h4>
+                  <div className="mx-auto flex items-center justify-center md:block lg:flex">
+                    <Formik
+                      initialValues={{
+                        name: "",
+                        email: "",
+                        message: "",
+                      }}
+                      validationSchema={validate}
+                      onSubmit={() => {
+                        //
+                      }}
+                    >
+                      {(formik) => (
+                        <form
+                          id="contact-form"
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          ref={formRef}
+                          onSubmit={submitForm}
+                        >
+                          <div className="mt-20 flex flex-col gap-10 text-gray-200">
+                            <input
+                              className="rounded-lg"
+                              name="name"
+                              type="text"
+                              placeholder="Name *"
+                            />
+                            <input
+                              className="rounded-lg"
+                              name="email"
+                              type="email"
+                              placeholder="Email Address *"
+                            />
+                            <input
+                              className="rounded-lg"
+                              name="message"
+                              type="text"
+                              placeholder="Your message here ..."
+                            />
+                          </div>
+                          <ReCaptcha
+                            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                            onChange={onCaptchaChange}
+                            className=" my-4 mt-10 w-full md:my-6"
+                          />
+                          <button
+                            type="submit"
+                            className="rounded-xl border border-black bg-transparent py-3 px-12 font-dmsans text-lg text-[#1F1A17]"
                           >
-                            {(formik) => (
-                              <form
-                                id="contact-form"
-                                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                                // @ts-ignore
-                                ref={formRef}
-                                onSubmit={submitForm}
-                              >
-                                <div className="text-gray-200">
-                                  <Input
-                                    name="name"
-                                    type="text"
-                                    placeholder="Name *"
-                                  />
-                                  <Input
-                                    name="email"
-                                    type="email"
-                                    placeholder="Email Address *"
-                                  />
-                                  <Input
-                                    name="message"
-                                    type="text"
-                                    placeholder="Your message here ..."
-                                  />
-                                </div>
-                                <ReCaptcha
-                                  sitekey={
-                                    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
-                                  }
-                                  onChange={onCaptchaChange}
-                                  className=" my-4 w-full md:my-6"
-                                />
-                                <button
-                                  type="submit"
-                                  className="rounded-xl border border-black bg-transparent py-3 px-12 font-dmsans text-lg text-[#1F1A17]"
-                                >
-                                  Send Message
-                                </button>
-                              </form>
-                            )}
-                          </Formik>
-                        </div>
-                      </div>
-                    </article>
-                  </section>
+                            Send Message
+                          </button>
+                        </form>
+                      )}
+                    </Formik>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
