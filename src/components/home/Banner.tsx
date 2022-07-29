3a3a4990ce7a60d42a1f0aefef6d1fe91d61c610
lorrainemakuyana <lorrainemakuyana23@gmail.com>
2022-07-29 11:56:02 +0000

    Update changes

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,8 +1,9 @@
-import Demo from "features/homepage/components/Demo";
 import { useRouter } from "next/router";
+import { isMobile } from "react-device-detect";
+
+import TrydemoLink from "../buttons/TrydemoLink";
 
 import Vector1SVG from "~/svg/homepage/vector_1.svg";
-
 export default function Banner() {
   const router = useRouter();
 
@@ -13,27 +14,33 @@ export default function Banner() {
           <Vector1SVG className="absolute z-0 -mt-10 h-[40rem] w-[40rem]" />
         </div>
         <div className="z-30 m-auto w-4/5 pt-40 lg:w-3/5 lg:pt-44">
-          <h1 className="relative text-8xl font-bold">Magic Mynt.</h1>
-          <p className="relative border-b border-black py-10 text-2xl font-medium">
+          <h1 className="relative z-30 text-8xl font-bold">Magic Mynt.</h1>
+          <p className="relative z-30 border-b border-black py-10 text-2xl font-medium">
             No-code tool for creating, launching and managing your NFT
             collection
           </p>
-          <div className="flex gap-5">
+          <div className="flex justify-between gap-5 md:justify-start">
             <div
-              className="relative mt-12 w-fit rounded-xl border border-black bg-transparent px-10 py-4 font-bold text-black transition-all hover:scale-105 hover:cursor-pointer"
+              className="relative z-30 mt-12 w-fit rounded-xl border border-black bg-transparent px-7 py-4 font-bold text-black transition-all hover:scale-105 hover:cursor-pointer md:mr-10 md:px-10 lg:mr-6"
               onClick={() => {
-                process.env.NEXT_PUBLIC_ENVIRONMENT == "development"
-                  ? router.push("/dashboard")
-                  : window.open("https://r3c9oapreew.typeform.com/to/RDOUdJXk");
+                if (isMobile) {
+                  alert(
+                    "This software does not work properly work on a mobile device. Please switch to another device and try again."
+                  );
+                } else {
+                  process.env.NEXT_PUBLIC_ENVIRONMENT == "development"
+                    ? router.push("/dashboard")
+                    : window.open(
+                        "https://r3c9oapreew.typeform.com/to/RDOUdJXk"
+                      );
+                }
               }}
             >
               {process.env.NEXT_PUBLIC_ENVIRONMENT == "development"
                 ? "Start Now"
                 : "Join Waitlist"}
             </div>
-
-            <Demo />
-
+            <TrydemoLink className="relative z-30 mt-12 w-fit rounded-xl border border-black bg-transparent px-7 py-4 font-bold text-black transition-all hover:scale-105 hover:cursor-pointer md:px-10" />
             {/* {process.env.NEXT_PUBLIC_ENVIRONMENT == "production" && (
               <div
                 className="relative z-30 mt-12 w-fit rounded-xl border border-black bg-transparent px-10 py-4 font-bold text-black transition-all hover:scale-105 hover:cursor-pointer"
@@ -41,7 +48,7 @@ export default function Banner() {
                   window.open("https://staging.magicmynt.com/dashboard");
                 }}
               >
-                Try Demo
+                <TrydemoLink />
               </div>
             )} */}
           </div>
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,7 +1,7 @@
-import ContactUs from "features/launch/components/contact-us";
 import { useRouter } from "next/router";
 import React from "react";
 
+import ContactLink from "../buttons/ContactLink";
 import UnstyledLink from "../links/UnstyledLink";
 
 import StarSVG from "~/svg/homepage/header_star.svg";
@@ -15,10 +15,10 @@ export default function Header() {
       <UnstyledLink href="/" className="w-fit text-xl ">
         Magic Mynt
       </UnstyledLink>
-      <span className="w-6 sm:w-1/5 md:inline md:w-2/5 lg:w-1/2 xl:w-3/5"></span>
-      <div className="hidden flex-row text-base md:flex lg:ml-0">
+      <span className="flex w-6 justify-end sm:w-1/5 md:inline md:w-1/3 lg:w-1/3 xl:w-1/2 2xl:w-3/5"></span>
+      <div className="mr-8 hidden flex-row justify-end text-base md:ml-12 md:flex lg:ml-0">
         <span
-          className=" cursor-pointer sm:mr-10 lg:mr-14"
+          className=" w-fit cursor-pointer sm:mr-10 lg:mr-14"
           onClick={() => {
             process.env.NEXT_PUBLIC_ENVIRONMENT == "development"
               ? router.push("/dashboard")
@@ -29,7 +29,7 @@ export default function Header() {
             ? "Get Started"
             : "Join Waitlist"}
         </span>
-        <ContactUs />
+        <ContactLink className="w-fit cursor-pointer" />
       </div>
     </div>
   );
