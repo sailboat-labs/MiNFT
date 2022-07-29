cbca5d50fbcca8b6157baefdb5b63113ab566769
Obitrim <pevangelist516@gmail.com>
2022-07-29 18:42:00 +0000

    feature: improved verify whitelist page

--- .Work/MiNFT/src/pages/indiansnft/whitelist/verify.tsx
+++ .Work/MiNFT/src/pages/indiansnft/whitelist/verify.tsx
@@ -7,8 +7,9 @@ import PageLoader from "@/components/shared/PageLoader";
 export default function WhitelistVerify() {
   // list of addresses to be whitelisted
   const [address, setAddress] = useState<string>("");
-  const [isVerified, setIsVerified] = useState<boolean | null>(null);
   const [isChecking, setIsChecking] = useState(false);
+  const [inputInFocus, setInputInFocus] = useState<boolean>(false);
+  const [isVerified, setIsVerified] = useState<boolean | null>(null);
 
   async function handleVerifyWhitelistAddress() {
     setIsChecking(true);
@@ -18,57 +19,124 @@ export default function WhitelistVerify() {
   }
 
   return (
-    <div className="container mx-auto mt-20 flex flex-col items-center justify-center">
-      {/* <Banner /> */}
-      <div className="text-3xl">Check your whitelist status</div>
-      {isVerified != null && (
-        <div
-          className={`mt-5 w-fit rounded-lg border-2 px-20  py-3 transition-all ${
-            isVerified
-              ? "border-green-200 bg-green-300 text-green-700"
-              : "border-red-200 bg-red-300 text-red-700"
-          }`}
-        >
-          {isVerified
-            ? "Account has been whitelisted!"
-            : "Account not whitelisted"}
-        </div>
-      )}
-      <div className="mt-10 text-gray-500">
-        Enter Wallet Address (not ENS name)
-      </div>
-      <input
-        disabled={isChecking}
-        onChange={(e) => {
-          setAddress(e.target.value);
-        }}
-        className="min-w-[500px] rounded border-2 px-5 py-2 text-center"
-        placeholder="Enter Wallet Address..."
-      />
-      {isChecking ? (
-        <PageLoader className="mt-10" />
-      ) : (
-        <div
-          onClick={() => {
-            handleVerifyWhitelistAddress();
-          }}
-          className="gradient-button mt-10"
-        >
-          Check Status
-        </div>
-      )}
-
-      <div className="absolute bottom-0 hidden flex-row justify-center overflow-hidden lg:flex">
-        {[...Array(9)].map((_, index) => (
-          <div key={index} className="box-border ">
-            <img
-              src={`/images/landing/indiansnfts/${index + 1}.png`}
-              alt="Indian NFT Example"
-              className="object-cover lg:h-52 lg:w-52"
-            />
+    <section className="relative w-screen overflow-x-hidden">
+      <div className="container mx-auto flex min-h-screen items-center justify-center">
+        {/* <Banner /> */}
+        <div className="hidden h-[600px] w-[400px] -translate-x-1/3 transform lg:flex">
+          <div className="flex flex-col justify-between">
+            {Array(3)
+              .fill(null)
+              .map((_, index) => (
+                <div key={index} className="inline-block overflow-x-hidden">
+                  <img
+                    src={`/images/landing/indiansnfts/${index + 1}.png`}
+                    alt="Indian NFT Example"
+                    className="-ml-1 object-cover lg:h-44 lg:w-44"
+                  />
+                </div>
+              ))}
           </div>
-        ))}
+          <div className="flex flex-col justify-center gap-10">
+            {Array(2)
+              .fill(null)
+              .map((_, index) => (
+                <div key={index} className="inline-block overflow-x-hidden">
+                  <img
+                    src={`/images/landing/indiansnfts/${index + 3}.png`}
+                    alt="Indian NFT Example"
+                    className="-ml-1 object-cover lg:h-44 lg:w-44"
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="-translate-y-56 transform md:-translate-y-24">
+          <div className="text-center text-4xl">
+            Check your whitelist status
+          </div>
+          {isVerified != null && (
+            <div
+              className={`mt-5 w-fit rounded-lg border-2 px-20  py-3 transition-all ${
+                isVerified
+                  ? "border-green-200 bg-green-300 text-green-700"
+                  : "border-red-200 bg-red-300 text-red-700"
+              }`}
+            >
+              {isVerified
+                ? "Account has been whitelisted!"
+                : "Account not whitelisted"}
+            </div>
+          )}
+          <div
+            className={`mx-2 mt-12 flex overflow-hidden rounded-full p-1 ring-2  ${
+              inputInFocus ? "ring-blue-400" : "ring-gray-200"
+            }`}
+          >
+            <input
+              disabled={isChecking}
+              onBlur={() => setInputInFocus(false)}
+              onFocus={() => setInputInFocus(true)}
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+              className="flex-1 rounded-full px-5 py-2 text-center focus:outline-none focus:ring-0 md:min-w-[370px]"
+              placeholder="enter wallet Address..."
+            />
+            {isChecking ? (
+              <PageLoader className="hover:cursor-not-allowed" />
+            ) : (
+              <button
+                disabled={isChecking}
+                onClick={() => {
+                  handleVerifyWhitelistAddress();
+                }}
+                className="gradient-button rounded-full hover:mr-1"
+              >
+                Check Status
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="absolute bottom-0 flex max-h-[200px]  flex-wrap justify-center gap-2 overflow-hidden sm:max-h-[300px] lg:hidden">
+          {[...Array(9)].map((_, index) => (
+            <div key={index} className="box-border ">
+              <img
+                src={`/images/landing/indiansnfts/${index + 1}.png`}
+                alt="Indian NFT Example"
+                className="h-52 w-52 object-contain"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="hidden h-[600px] w-[400px] translate-x-1/3 transform flex-row-reverse lg:flex">
+          <div className="flex flex-col justify-between">
+            {Array(3)
+              .fill(null)
+              .map((_, index) => (
+                <div className="inline-block overflow-x-hidden" key={index}>
+                  <img
+                    src={`/images/landing/indiansnfts/${index + 6}.png`}
+                    alt="Indian NFT Example"
+                    className="-ml-1 object-cover lg:h-44 lg:w-44"
+                  />
+                </div>
+              ))}
+          </div>
+          <div className="flex flex-col justify-center gap-10">
+            {Array(2)
+              .fill(null)
+              .map((_, index) => (
+                <div className="inline-block overflow-x-hidden" key={index}>
+                  <img
+                    src={`/images/landing/indiansnfts/${index + 5}.png`}
+                    alt="Indian NFT Example"
+                    className="-ml-1 object-cover lg:h-44 lg:w-44"
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
