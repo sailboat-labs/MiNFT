fd44065d781320347f1838e0d1978b5bb5944012
Obitrim <pevangelist516@gmail.com>
2022-08-02 03:38:43 +0000

    feature: rendered page skeleton

--- features/launch/launch/components/launchpad/launchpad.tsx
+++ features/launch/launch/components/launchpad/launchpad.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
+import { Icon } from "@iconify/react";
 import { doc, onSnapshot } from "firebase/firestore";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 import PageLoader from "@/components/shared/PageLoader";
+import LaunchPadSkeleton from "@/components/skeletons/launchpad";
 
 import { IProjectLaunch } from "@/interfaces";
 import { firestore } from "@/pages/dashboard";
@@ -64,7 +66,7 @@ const ProjectLaunch: NextPage = () => {
       <LaunchHeader />
       {launchInformation ? (
         <div className="h-screen overflow-y-auto">
-          <div className="">
+          <div>
             <section>
               <div className="container mx-auto mt-20 max-w-[1664px]  px-6 py-24 lg:grid lg:grid-cols-2">
                 {/* left side */}
@@ -101,20 +103,10 @@ const ProjectLaunch: NextPage = () => {
                           }}
                           className="flex w-fit cursor-pointer items-center gap-1 rounded-xl border-2 py-1 pl-2 pr-4 text-sm transition-transform hover:scale-105"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                            />
-                          </svg>
+                          <Icon
+                            icon="ant-design:twitter-outlined"
+                            className="text-xl"
+                          />
                           Twitter
                         </div>
                       )}
@@ -124,22 +116,12 @@ const ProjectLaunch: NextPage = () => {
                           onClick={() => {
                             window.open(launchInformation.openseaLink);
                           }}
-                          className="flex w-fit cursor-pointer items-center gap-1 rounded-xl border-2 py-1 pl-2 pr-4 text-sm transition-transform hover:scale-105"
+                          className="group flex w-fit cursor-pointer items-center gap-1 rounded-xl border-2 py-1 pl-2 pr-4 text-sm transition-transform hover:scale-105"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                            />
-                          </svg>
+                          <Icon
+                            icon="simple-icons:opensea"
+                            className="text-lg"
+                          />
                           Opensea
                         </div>
                       )}
@@ -151,20 +133,10 @@ const ProjectLaunch: NextPage = () => {
                           }}
                           className="flex w-fit cursor-pointer items-center gap-2 rounded-xl border-2 py-1 pl-2 pr-4 text-sm transition-transform hover:scale-105"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                            />
-                          </svg>
+                          <Icon
+                            icon="fa-brands:discord"
+                            className="text-base"
+                          />
                           Discord
                         </div>
                       )}
@@ -199,24 +171,18 @@ const ProjectLaunch: NextPage = () => {
                           }}
                           className="flex w-fit cursor-pointer items-center gap-2 rounded-xl border-2 py-1 pl-2 pr-4 text-sm transition-transform hover:scale-105"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
+                          <Icon
+                            icon="clarity:contract-solid"
+                            className="text-base"
+                          />
                           Contract
                         </div>
                       )}
                   </div>
-                  <p className="my-4">{launchInformation.summary}</p>
 
+                  <p className="mt-8 text-lg font-medium">
+                    {launchInformation.summary}
+                  </p>
                   <div className="mt-6 grid grid-rows-2 gap-5">
                     {launchInformation.hasWhitelist && (
                       <div className=" rounded-2xl  p-4 ring-1 ring-gray-200">
@@ -388,7 +354,7 @@ const ProjectLaunch: NextPage = () => {
           </div>
         </div>
       ) : (
-        <div></div>
+        <LaunchPadSkeleton />
       )}
     </>
   );
